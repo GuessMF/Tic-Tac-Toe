d883a55f3326e12dfa44bb570b35760d3ea8e27e
GuessMF <segas95@yandex.ru>
2022-09-19 13:12:39 +0300

    master

---
+++ index.js
@@ -0,0 +1,46 @@
+let squares = document.querySelectorAll("#table td");
+start(squares);
+
+function start(squares) {
+  let y = 0;
+  let show = document.getElementById("table");
+
+  for (let square of squares) {
+    square.addEventListener("click", function click() {
+      this.textContent = ["x", "o"][y % 2];
+      this.removeEventListener("click", click);
+      y++;
+      if (isVictory(squares) && this.textContent == "x") {
+        show.textContent = "Победил игрок 1";
+      } else if (isVictory(squares) && this.textContent == "o") {
+        show.textContent = "Победил игрок 2";
+      } else if (y == 9) {
+        show.textContent = "Ничья";
+      }
+    });
+  }
+}
+
+function isVictory(squares) {
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8],
+  ];
+
+  for (let win of wins) {
+    if (
+      squares[win[0]].textContent == squares[win[1]].textContent &&
+      squares[win[1]].textContent == squares[win[2]].textContent &&
+      squares[win[0]].textContent != ""
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
